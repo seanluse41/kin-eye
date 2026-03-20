dab6354fdfe94a26089033eae33ad3a3496d23eb
seanluse41 <seanluse41@gmail.com>
2026-03-20 13:25:53 +0900

    structure and i18n

---
+++ src/lib/i18n/index.js
@@ -0,0 +1,12 @@
+// src/lib/i18n/index.js
+import { init, register } from 'svelte-i18n'
+
+const defaultLocale = 'ja'
+
+register('en', () => import('./locales/en.json'))
+register('ja', () => import('./locales/ja.json'))
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: defaultLocale,
+})
