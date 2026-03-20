04d643d76a205dc9a5c0499d0c28c6d372873957
seanluse41 <seanluse41@gmail.com>
2026-03-20 13:03:04 +0900

    deep link and single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,35 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{Manager};
+use tauri_plugin_deep_link::DeepLinkExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
-        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            #[cfg(any(windows, target_os = "linux"))]
+            app.deep_link().register_all()?;
+
+            app.deep_link().on_open_url(|event| {
+                println!("hello from deep link");
+                println!("urls: {:?}", event.urls());
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
